78083b319b1f03839a71e8ef967aa675e9781d77
XenonicDev <andrewdepke@gmail.com>
2018-10-21 14:39:36 -0600

    Converted TaskQueue to Class Template

--- TaskQueue/Include/TaskQueue.h
+++ TaskQueue/Include/TaskQueue.h
@@ -10,16 +10,17 @@
 #include <functional>
 #include <vector>
 #include <thread>
+#include <cassert>
+#include <algorithm>
 
+template <typename T>
 class TaskQueue
 {
-	typedef std::function<void(void)> TaskType;
-
 protected:
 	std::mutex Lock;
 	std::condition_variable WorkerSignal;
 
-	std::deque<std::pair<int, TaskType>> Queue;
+	std::deque<std::pair<int, T>> Queue;
 	int CurrentWorkID;
 
 	std::vector<std::pair<std::thread, bool>> Workers;
@@ -40,7 +41,7 @@ public:
 	void Resize(int NewWorkerCount);
 
 	// Queue up a new task. Returns the task ID.
-	int Enqueue(TaskType Task);
+	int Enqueue(T Task);
 
 	// Returns how many tasks are in the queue waiting to start.
 	int GetUnstartedTasksCount();
@@ -61,5 +62,195 @@ protected:
 	bool IsPendingDestroy(std::thread::id ID);
 
 	// [Not Locked]
-	TaskType Dequeue();
+	T Dequeue();
 };
+
+template <typename T>
+void TaskQueue<T>::WorkerRunnable(TaskQueue* const Manager)
+{
+	// Worker execution loop.
+	while (Manager && !Manager->IsPendingDestroy(std::this_thread::get_id()))
+	{
+		// We have to lock here, otherwise if work is queued after we find that there's an empty work queue, but before we obtain a
+		// lock to begin sleeping, we won't wait on the condition variable in time and will miss the queue up.
+		std::unique_lock<std::mutex> EvaluationLock(Manager->Lock);
+
+		auto NewTask = Manager->Dequeue();
+
+		// We have a task, run it.
+		if (NewTask)
+		{
+			EvaluationLock.unlock();
+
+			std::invoke(NewTask);
+		}
+
+		// No new tasks available, sleep until we receive a signal.
+		else
+		{
+			// Sleep until new work is queued. Once awoken, the worker performs a state reevaluation before pulling from the work queue.
+			WorkerSignal.wait(EvaluationLock);
+		}
+	}
+}
+
+template <typename T>
+void TaskQueue<T>::DeployWorker()
+{
+	Workers.push_back({ std::thread(std::bind(&TaskQueue::WorkerRunnable, this, this)), false });
+}
+
+template <typename T>
+void TaskQueue<T>::KillWorker()
+{
+	Workers.back().second = true;
+	Workers.back().first.detach();
+
+	Workers.pop_back();
+}
+
+template <typename T>
+TaskQueue<T>::TaskQueue() {}
+
+template <typename T>
+TaskQueue<T>::TaskQueue(int WorkerCount)
+{
+	assert(WorkerCount > 0 && "WorkerCount must be greater than 0!");
+
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	// Launch the workers.
+	for (int Iter = 0; Iter < WorkerCount; ++Iter)
+	{
+		DeployWorker();
+	}
+}
+
+template <typename T>
+int TaskQueue<T>::GetWorkerCount()
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	return Workers.size();
+}
+
+template <typename T>
+void TaskQueue<T>::Resize(int NewWorkerCount)
+{
+	assert(NewWorkerCount > 0 && "WorkerCount must be greater than 0!");
+
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	if (NewWorkerCount > static_cast<int>(Workers.size()))
+	{
+		for (int Iter = Workers.size(); Iter < NewWorkerCount; ++Iter)
+		{
+			DeployWorker();
+		}
+	}
+
+	else if (NewWorkerCount < static_cast<int>(Workers.size()))
+	{
+		for (int Iter = Workers.size() - NewWorkerCount; Iter > 0; --Iter)
+		{
+			KillWorker();
+		}
+
+		// Force a reevaluation on all workers.
+		WorkerSignal.notify_all();
+	}
+}
+
+template <typename T>
+int TaskQueue<T>::Enqueue(T Task)
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	++CurrentWorkID;
+
+	Queue.push_back({ CurrentWorkID, Task });
+
+	WorkerSignal.notify_one();
+
+	return CurrentWorkID;
+}
+
+template <typename T>
+int TaskQueue<T>::GetUnstartedTasksCount()
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	return Queue.size();
+}
+
+template <typename T>
+bool TaskQueue<T>::CancelUnstartedTask(int ID)
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	auto Task = std::find_if(Queue.cbegin(), Queue.cend(), [ID](const auto& Arg) { return Arg.first == ID; });
+	if (Task != Queue.cend())
+	{
+		Queue.erase(Task);
+
+		return true;
+	}
+
+	return false;
+}
+
+template <typename T>
+void TaskQueue<T>::CancelAllUnstartedTasks()
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	Queue.clear();
+}
+
+template <typename T>
+void TaskQueue<T>::Stop()
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	Queue.clear();
+
+	for (auto& Worker : Workers)
+	{
+		Worker.second = true;
+		Worker.first.detach();
+	}
+
+	// Make sure we kill off any sleeping workers.
+	WorkerSignal.notify_all();
+}
+
+template <typename T>
+bool TaskQueue<T>::IsPendingDestroy(std::thread::id ID)
+{
+	std::lock_guard<std::mutex> LocalLock(Lock);
+
+	for (const auto& Worker : Workers)
+	{
+		if (Worker.first.get_id() == ID)
+		{
+			return Worker.second;
+		}
+	}
+
+	// This could be called by an unowned worker thread, so default to true.
+	return true;
+}
+
+template <typename T>
+T TaskQueue<T>::Dequeue()
+{
+	if (Queue.size() > 0)
+	{
+		auto Task = Queue.front();
+		Queue.pop_front();
+
+		return Task.second;
+	}
+
+	return {};
+}
